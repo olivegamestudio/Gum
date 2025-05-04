054592726ecda84a47804e3b841ff606d8e76e4a
Victor Chelaru <VicChelaru@gmail.com>
2025-05-04 04:50:42 -0600

    More work getting FNA editor tab to compile

---
+++ Tool/FnaTest1/Services/LayerService.cs
@@ -0,0 +1,34 @@
+﻿using RenderingLibrary.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gum.Plugins.InternalPlugins.EditorTab.Services;
+public class LayerService
+{
+    public Layer OverlayLayer { get; private set; }
+    public Layer MainEditorLayer { get; private set; }
+
+    public Layer RulerLayer { get; private set; }
+
+    public LayerService()
+    {
+    }
+
+    public void Initialize()
+    {
+        MainEditorLayer = Renderer.Self.AddLayer();
+        MainEditorLayer.Name = "Main Editor Layer";
+
+
+        OverlayLayer = Renderer.Self.AddLayer();
+        OverlayLayer.Name = "Overlay Layer";
+
+        RulerLayer = Renderer.Self.AddLayer();
+        RulerLayer.LayerCameraSettings = new LayerCameraSettings();
+        RulerLayer.LayerCameraSettings.IsInScreenSpace = true;
+        RulerLayer.Name = "Ruler Layer";
+    }
+}
